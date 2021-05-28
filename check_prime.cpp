0e722903481ce55c19572a8fcc19c0d29e06e522
arpit1397 <59012646+arpit1397@users.noreply.github.com>
2021-05-28 19:45:43 +0530

    Create check_prime.cpp

---
+++ check_prime.cpp
@@ -0,0 +1,23 @@
+bool check_prime(long long n)
+{
+	int flag = 0;
+	for (long long i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			flag = 1;
+			break;
+		}
+	}
+	if (n == 1)
+		return false;
+	else if (flag == 0 || n == 2 || n == 3)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+
+}
