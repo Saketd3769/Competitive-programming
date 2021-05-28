d1eb33f07b0488e7c6f47f1b5590f1a98c0e99dc
arpit1397 <59012646+arpit1397@users.noreply.github.com>
2021-05-28 19:43:38 +0530

    Create Binary_exponentiation.cpp

---
+++ Binary_exponentiation.cpp
@@ -0,0 +1,12 @@
+int BE(int x, int n, int m) //function to calculate x raise to power n modulo m
+{
+	int result = 1;
+	while (n > 0)
+	{
+		if (n % 2 == 1)
+			result = result * x % m;
+		x = x * x % m;
+		n = n / 2;
+	}
+	return result;
+}
